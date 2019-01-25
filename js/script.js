02e70d05dcc944f74c4a9cb42935224adad8ab26
Toto Corvidoni <totocorvidoni@gmail.com>
2019-01-25 16:22:25 -0300

    Add win check and refactor board logic.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const cells = document.querySelectorAll('.cell');
 
 const Players = (() => {
-  let one;
-  let two;
-  let active;
-  const getFirst = () => one;
-  const getSecond = () => two;
-  const getActive = () => active;
-  const create = function(name, mark) { return {name, mark} };
+  let one
+  let two
+  let active
+  const getFirst = () => one
+  const getSecond = () => two
+  const getActive = () => active
+  const create = function(name, mark) { return {name, mark} }
   const add = (name, mark) => {
-    player = create(name, mark);
+    player = create(name, mark)
     if (one === undefined) {
       one = player;
       active = one
@@ -17,34 +17,47 @@ const Players = (() => {
       two = player
     } 
   }
-  const swap = () => active = (active === one) ? two : one;
-
+  const swap = () => active = (active === one) ? two : one
   return {getFirst, getSecond, getActive,add, swap}
 })();
 
 const GameBoard = (() => {
-  let board = [['', '', ''],
-               ['', '', ''],
-               ['', '', '']];
-  // creates the DOM board using the board variable.
+  let board = ['', '', '', '', '', '', '', '', '']
+  const wins = [[0,1,2], [3,4,5], [6,7,8],
+                [0,4,8], [2,4,6], [0,3,6],
+                [1,4,7], [2,5,8]]
+  // places the mark on the browser's cell.
   const render = (cell) => {
-    let mark = board[cell.dataset.cellId[0]][cell.dataset.cellId[1]]
+    let mark = board[cell.dataset.cellId]
     if(mark !== '') {
       let element = document.createElement('img');
       element.setAttribute('src', `images/${mark}.svg`);
       cell.appendChild(element);
     }
   };
-  const addMark = (cord, mark) => {
-    board[cord[0]][cord[1]] = mark;
-  }
   const reset = () => {
-    board = [['', '', ''], ['', '', ''], ['', '', '']];
+    let board = ['', '', '', '', '', '', '', '', ''];
     cells.forEach((cell) => {
       cell.innerHTML = '';
     })
   }
-  return {render,reset, addMark}
+  const addMark = (spot, mark) => {
+    board[spot] = mark;
+  }
+
+  const winCheck = () => {
+    let winner;
+    wins.forEach(cond => {
+      win = [board[cond[0]], board[cond[1]], board[cond[2]]];
+      if (win.every(cell => cell == 'x')) {
+        winner = 'x';
+      } else if (win.every(cell => cell == 'o')) {
+        winner = 'o'
+      }
+    })
+    return winner;
+  }
+  return {render,reset, addMark, winCheck}
 })();
 
 
